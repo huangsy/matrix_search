134dbbe0c1f75b58c55cb91438ed0629766a45ba
huangshiyu@youzan.com <huangshiyu@youzan.com>
2016-10-26 01:21:49 +0800

    二分查找

--- MatrixSearch.js
+++ MatrixSearch.js
@@ -1,4 +1,5 @@
 module.exports = {
+    // 二分查找，递归
     binary: function(array, from, to, target) {
         var mid;
         if (from <= to) {
@@ -14,8 +15,52 @@ module.exports = {
             return false;
         }
     },
+
+    // 获取所在行数
+    getRowNum: function(matrix, target) {
+        var from = 0;
+        var to = matrix.length - 1;
+        var rowSize = to;
+        var rowNum = -1;
+        // 二分查找，非递归
+        while (from !== to) {
+            var mid = parseInt((from + to) / 2);
+            var midRow = matrix[mid];
+            var prevRow = matrix[mid - 1];
+            var nextRow = matrix[mid + 1];
+            if (midRow[0] <= target && midRow[midRow.length - 1] >= target) {
+                // 在中间一行
+                rowNum = mid;
+                break;
+            } else if (mid === 1 && prevRow[0] >= target && prevRow[prevRow.length - 1] <= target ) {
+                // 在第一行
+                rowNum = 0;
+                break;
+            } else if (mid + 1 === rowSize && nextRow[0] <= target && nextRow[nextRow.length - 1] >= target) {
+                // 在最后一行
+                rowNum = rowSize;
+                break; 
+            } else if (mid > 1 && prevRow[prevRow.length - 1] >= target) {
+                // 在中间一行前面
+                to = mid - 1;
+            } else if (mid + 1 < rowSize && nextRow[0] <= target) {
+                // 在中间一行后面
+                from = mid + 1;
+            } else {
+                break;
+            }
+        }
+        if (from === to) {
+            rowNum = from;
+        }
+        return rowNum;
+    },
+
     search: function(matrix, target) {
-        var rowSize = matrix.length;
-        return result = this.binary([1,2,4], 0, 2, target);
+        var rowNum = this.getRowNum(matrix, target);
+        if (rowNum < 0) {
+            return false;
+        }
+        return this.binary(matrix[rowNum], 0, matrix[rowNum].length - 1, target);
     }
-};;
+};
--- index.js
+++ index.js
@@ -3,8 +3,32 @@ var should = require('should');
 var matrix = [
     [2,   4,  8,  9],
     [10, 13, 15, 21],
-    [23, 31, 33, 51]
+    [23, 31, 33, 51],
+    [53, 61, 63, 71],
+    [73, 81, 83, 91],
+    [93, 101, 103, 121]
 ];
 
+MatrixSearch.getRowNum(matrix, 1).should.be.exactly(-1);
+MatrixSearch.search(matrix, 1).should.be.exactly(false);
+
+MatrixSearch.getRowNum(matrix, 4).should.be.exactly(0);
 MatrixSearch.search(matrix, 4).should.be.exactly(true);
+
+MatrixSearch.getRowNum(matrix, 3).should.be.exactly(0);
 MatrixSearch.search(matrix, 3).should.be.exactly(false);
+
+MatrixSearch.getRowNum(matrix, 22).should.be.exactly(-1);
+MatrixSearch.search(matrix, 22).should.be.exactly(false);
+
+MatrixSearch.getRowNum(matrix, 60).should.be.exactly(3);
+MatrixSearch.search(matrix, 60).should.be.exactly(false);
+
+MatrixSearch.getRowNum(matrix, 81).should.be.exactly(4);
+MatrixSearch.search(matrix, 81).should.be.exactly(true);
+
+MatrixSearch.getRowNum(matrix, 100).should.be.exactly(5);
+MatrixSearch.search(matrix, 100).should.be.exactly(false);
+
+MatrixSearch.getRowNum(matrix, 130).should.be.exactly(-1);
+MatrixSearch.search(matrix, 130).should.be.exactly(false);
